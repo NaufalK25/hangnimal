4373ec5a13e1651821f6547068bbb962f920bfcd
NaufalK25 <naufalkateni2001@gmail.com>
2022-03-22 11:40:28 +0700

    make the answer cant be seen by inspect and block

--- public/js/index.ts
+++ public/js/index.ts
@@ -2,45 +2,39 @@ const DURATION = 5000;
 
 // Get Random Animal
 const question = <HTMLElement>document.querySelector('section.question');
-const answer = <HTMLParagraphElement>document.querySelector('p.answer');
 
 fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
     .then(response => response.json())
     .then(animal => {
-        answer.textContent = animal.name.toUpperCase();
-
+        const realAnswer = animal.name.toUpperCase();
+        const dummyAnswer = realAnswer.replace(/[A-Z]/g, '?');
         const answerContainer = document.createElement('section');
         answerContainer.classList.add('answer-container');
-        answer.textContent?.split('').forEach(letter => {
-            const character = document.createElement('span');
-            character.classList.add('answer');
-            if (letter.match(/[a-zA-Z\s]/)) {
-                character.textContent = letter;
-            }
-            answerContainer.appendChild(character);
+        dummyAnswer.split('').forEach((character: string) => {
+            const answerCharacter = document.createElement('span');
+            answerCharacter.classList.add('answer');
+            answerCharacter.classList.add((character === '?') ? 'answer-dummy' : 'answer-special');
+            answerCharacter.textContent = character;
+            answerContainer.appendChild(answerCharacter);
         });
-        answer.remove();
         question.append(answerContainer);
 
 
         // Keyboard events
         const keyboardCharacters: NodeListOf<HTMLSpanElement> = document.querySelectorAll('span.key');
         const answers: NodeListOf<HTMLSpanElement> = document.querySelectorAll('span.answer');
-        const words: string[] = [];
+        const dummies: string[] = [];
+        const real = realAnswer.split('');
 
         answers.forEach(answer => {
             const character = answer.textContent?.toUpperCase();
             if (character) {
-                words.push(character);
+                dummies.push(character);
             }
         });
 
-        let correct = 0, incorrect = 0;
-        for (const word of words) {
-            if (word === ' ') {
-                correct++;
-            }
-        }
+        let correct = dummies.filter(dummy => dummy.match(/[^a-zA-Z?]/)).length;
+        let incorrect = 0;
 
         // Hangman
         const pole = <HTMLDivElement>document.querySelector('div.pole');
@@ -57,119 +51,31 @@ fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
                 if (!keyboardCharacter.classList.contains('correct') && !keyboardCharacter.classList.contains('incorrect')) {
                     const character = keyboardCharacter.dataset.key;
                     if (character) {
-                        if (words.includes(character)) {
+                        if (real.includes(character)) {
                             keyboardCharacter.classList.add('correct');
-                            answers.forEach(answer => {
-                                if (answer.textContent?.toUpperCase() === character) {
-                                    answer.style.color = 'black';
-                                    correct++;
-                                    if (correct === words.length) {
-                                        const middleLayer = document.createElement('section');
-                                        middleLayer.classList.add('middle-layer');
-                                        const result = document.createElement('p');
-                                        result.classList.add('result');
-                                        result.style.backgroundColor = 'lime';
-                                        result.textContent = 'You Win!';
-                                        middleLayer.appendChild(result);
-                                        document.body.appendChild(middleLayer);
+                            answers.forEach((answer, answerIndex) => {
+                                real.forEach((realAnswer: string, realAnswerIndex: number) => {
+                                    if (realAnswer === character) {
+                                        if (answerIndex === realAnswerIndex) {
+                                            answer.textContent = character;
+                                            correct++;
+                                            if (correct === real.length) {
+                                                const middleLayer = document.createElement('section');
+                                                middleLayer.classList.add('middle-layer');
+                                                const result = document.createElement('p');
+                                                result.classList.add('result');
+                                                result.style.backgroundColor = 'lime';
+                                                result.textContent = 'You Win!';
+                                                middleLayer.appendChild(result);
+                                                document.body.appendChild(middleLayer);
 
-                                        setTimeout(() => {
-                                            window.location.reload();
-                                        }, DURATION);
+                                                setTimeout(() => {
+                                                    window.location.reload();
+                                                }, DURATION);
+                                            }
+                                        }
                                     }
-                                }
-                            });
-                        } else {
-                            keyboardCharacter.classList.add('incorrect');
-                            incorrect++;
-                            if (incorrect === 1) {
-                                pole.style.display = 'flex';
-                                pole.style.justifyContent = 'center';
-                                pole.style.alignItems = 'center';
-                            } else if (incorrect === 2) {
-                                pole.remove();
-                                rope.style.display = 'flex';
-                                rope.style.justifyContent = 'center';
-                                rope.style.alignItems = 'center';
-                            } else if (incorrect === 3) {
-                                rope.remove();
-                                head.style.display = 'flex';
-                                head.style.justifyContent = 'center';
-                                head.style.alignItems = 'center';
-                            } else if (incorrect === 4) {
-                                head.remove();
-                                body.style.display = 'flex';
-                                body.style.justifyContent = 'center';
-                                body.style.alignItems = 'center';
-                            } else if (incorrect === 5) {
-                                body.remove();
-                                leftArm.style.display = 'flex';
-                                leftArm.style.justifyContent = 'center';
-                                leftArm.style.alignItems = 'center';
-                            } else if (incorrect === 6) {
-                                leftArm.remove();
-                                rightArm.style.display = 'flex';
-                                rightArm.style.justifyContent = 'center';
-                                rightArm.style.alignItems = 'center';
-                            } else if (incorrect === 7) {
-                                rightArm.remove();
-                                leftLeg.style.display = 'flex';
-                                leftLeg.style.justifyContent = 'center';
-                                leftLeg.style.alignItems = 'center';
-                            } else if (incorrect === 8) {
-                                leftLeg.remove();
-                                rightLeg.style.display = 'flex';
-                                rightLeg.style.justifyContent = 'center';
-                                rightLeg.style.alignItems = 'center';
-
-                                const middleLayer = document.createElement('section');
-                                middleLayer.classList.add('middle-layer');
-                                const result = document.createElement('p');
-                                result.classList.add('result');
-                                result.style.backgroundColor = 'red';
-                                result.innerText = `You Lose! The correct answer is ${words.join('')}`;
-                                middleLayer.appendChild(result);
-                                document.body.appendChild(middleLayer);
-
-                                setTimeout(() => {
-                                    window.location.reload();
-                                }, DURATION);
-                            }
-                        }
-                    }
-                }
-            });
-        });
-
-        window.addEventListener('keydown', (event) => {
-            const key = event.key.toUpperCase();
-            const keyboardCharacter = <HTMLSpanElement>document.querySelector(`span.key[data-key="${key}"]`);
-            if (keyboardCharacter) {
-                if (!keyboardCharacter.classList.contains('correct') && !keyboardCharacter.classList.contains('incorrect')) {
-                    keyboardCharacter.classList.add('key-pressed');
-                    const character = keyboardCharacter.dataset.key;
-                    if (character) {
-                        if (words.includes(character)) {
-                            keyboardCharacter.classList.add('correct');
-                            answers.forEach(answer => {
-                                if (answer.textContent?.toUpperCase() === character) {
-                                    answer.style.color = 'black';
-                                    correct++;
-                                    if (correct === words.length) {
-                                        const middleLayer = document.createElement('section');
-                                        middleLayer.classList.add('middle-layer');
-                                        const result = document.createElement('p');
-                                        result.classList.add('result');
-                                        result.style.backgroundColor = 'lime';
-                                        result.textContent = 'You Win!';
-                                        middleLayer.appendChild(result);
-                                        document.body.appendChild(middleLayer);
-
-                                        setTimeout(() => {
-                                            window.location.reload();
-                                        }, DURATION);
-                                    }
-                                }
+                                });
                             });
                         } else {
                             keyboardCharacter.classList.add('incorrect');
@@ -220,7 +126,104 @@ fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
                                 const result = document.createElement('p');
                                 result.classList.add('result');
                                 result.style.backgroundColor = 'red';
-                                result.innerText = `You Lose! The correct answer is ${words.join('')}`;
+                                result.innerText = `You Lose! The correct answer is ${real.join('')}`;
+                                middleLayer.appendChild(result);
+                                document.body.appendChild(middleLayer);
+
+                                setTimeout(() => {
+                                    window.location.reload();
+                                }, DURATION);
+                            }
+                        }
+                    }
+                }
+            });
+        });
+
+        window.addEventListener('keydown', (event) => {
+            const key = event.key.toUpperCase();
+            const keyboardCharacter = <HTMLSpanElement>document.querySelector(`span.key[data-key="${key}"]`);
+            if (keyboardCharacter) {
+                if (!keyboardCharacter.classList.contains('correct') && !keyboardCharacter.classList.contains('incorrect')) {
+                    keyboardCharacter.classList.add('key-pressed');
+                    const character = keyboardCharacter.dataset.key;
+                    if (character) {
+                        if (real.includes(character)) {
+                            keyboardCharacter.classList.add('correct');
+                            answers.forEach((answer, answerIndex) => {
+                                real.forEach((realAnswer: string, realAnswerIndex: number) => {
+                                    if (realAnswer === character) {
+                                        if (answerIndex === realAnswerIndex) {
+                                            answer.textContent = character;
+                                            correct++;
+                                            if (correct === real.length) {
+                                                const middleLayer = document.createElement('section');
+                                                middleLayer.classList.add('middle-layer');
+                                                const result = document.createElement('p');
+                                                result.classList.add('result');
+                                                result.style.backgroundColor = 'lime';
+                                                result.textContent = 'You Win!';
+                                                middleLayer.appendChild(result);
+                                                document.body.appendChild(middleLayer);
+
+                                                setTimeout(() => {
+                                                    window.location.reload();
+                                                }, DURATION);
+                                            }
+                                        }
+                                    }
+                                });
+                            });
+                        } else {
+                            keyboardCharacter.classList.add('incorrect');
+                            incorrect++;
+                            console.log(incorrect);
+                            if (incorrect === 1) {
+                                pole.style.display = 'flex';
+                                pole.style.justifyContent = 'center';
+                                pole.style.alignItems = 'center';
+                            } else if (incorrect === 2) {
+                                pole.remove();
+                                rope.style.display = 'flex';
+                                rope.style.justifyContent = 'center';
+                                rope.style.alignItems = 'center';
+                            } else if (incorrect === 3) {
+                                rope.remove();
+                                head.style.display = 'flex';
+                                head.style.justifyContent = 'center';
+                                head.style.alignItems = 'center';
+                            } else if (incorrect === 4) {
+                                head.remove();
+                                body.style.display = 'flex';
+                                body.style.justifyContent = 'center';
+                                body.style.alignItems = 'center';
+                            } else if (incorrect === 5) {
+                                body.remove();
+                                leftArm.style.display = 'flex';
+                                leftArm.style.justifyContent = 'center';
+                                leftArm.style.alignItems = 'center';
+                            } else if (incorrect === 6) {
+                                leftArm.remove();
+                                rightArm.style.display = 'flex';
+                                rightArm.style.justifyContent = 'center';
+                                rightArm.style.alignItems = 'center';
+                            } else if (incorrect === 7) {
+                                rightArm.remove();
+                                leftLeg.style.display = 'flex';
+                                leftLeg.style.justifyContent = 'center';
+                                leftLeg.style.alignItems = 'center';
+                            } else if (incorrect === 8) {
+                                leftLeg.remove();
+                                rightLeg.style.display = 'flex';
+                                rightLeg.style.justifyContent = 'center';
+                                rightLeg.style.alignItems = 'center';
+
+                                const middleLayer = document.createElement('section');
+                                middleLayer.classList.add('middle-layer');
+                                const result = document.createElement('p');
+                                result.classList.add('result');
+                                result.style.backgroundColor = 'red';
+                                result.innerText = `You Lose! The correct answer is ${real.join('')}`;
                                 middleLayer.appendChild(result);
                                 document.body.appendChild(middleLayer);
 
